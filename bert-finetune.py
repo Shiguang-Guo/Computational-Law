03ddfda5cb873c3a00deea334c1bd6457c8c4400
‘gsg-server’ <nb2000@yeah.net>
2021-07-02 09:04:00 +0800

    bert的多标签模型

--- bert-finetune.py
+++ bert-finetune.py
@@ -5,10 +5,13 @@
 # @Site : 
 # @File : bet-fimodelune.py
 # @Software: PyCharm
+import argparse
 import copy
 import json
 import re
+import warnings
 
+warnings.filterwarnings('ignore')  # "error", "ignore", "always", "default", "module" or "once"
 import numpy as np
 import torch
 import torch.nn as nn
@@ -16,13 +19,14 @@ import torch.optim as optim
 from torch.utils.data import DataLoader
 from torch.utils.data import Dataset
 from tqdm import tqdm
-from transformers import BertTokenizer, BertForSequenceClassification
+from transformers import BertModel, BertTokenizer
 from torch.utils.tensorboard import SummaryWriter
 from sklearn.metrics import f1_score
+import torch.nn.functional as F
 
 form_label_list = ["其他", "网上电子汇款", "现金", "未知或模糊", "网络贷款平台", "授权支配特定资金账户", "未出借", "银行转账", "票据"]
 borrower_attr_label_list = ["其他组织", "自然人", "法人"]
-use_label_list = ["个人生活", "其他", "夫妻共同生活", "违法犯罪", "企业生产经营"],
+use_label_list = ["个人生活", "其他", "夫妻共同生活", "违法犯罪", "企业生产经营"]
 proof_label_list = ["其他", "未知或模糊", "还款承诺", "收据、收条", "欠条", "担保", "微信、短信、电话等聊天记录", "借款合同、借条、借据"]
 lender_attr_label_list = ["其他组织", "自然人", "法人"]
 intention_label_list = ["转贷牟利", "其他", "正常出借"]
@@ -36,12 +40,12 @@ name_dict = {
     'borrower_attr': "借款人基本属性",
     'use': "借款用途",
     'proof': "借贷合意的凭据",
-    'lender': "出借人基本属性",
+    'lender_attr': "出借人基本属性",
     'intention': "出借意图",
     'type': "担保类型",
     'rate': "约定期内利率（换算成年利率）",
     'calculation_method': "约定计息方式",
-    'repayment': "还款交付形式",
+    'repayment': "还款交付形式"
 }
 
 
@@ -72,8 +76,7 @@ class TaskDataset(Dataset):
         self.name = config['name']
         self.chinesename = name_dict[self.name]
         self.tokenizer = BertTokenizer.from_pretrained(config['bert_path'])
-        # self.labels = np.array([self.onehot(item['attr'][self.chinesename], self.name) for item in self.raw_data])
-        self.labels = np.array([self.getlabel(item['attr'][self.chinesename], self.name) for item in self.raw_data])
+        self.labels = np.array([self.onehot(item['attr'][self.chinesename], self.name) for item in self.raw_data])
 
         input_ids = []
         bert_attention_masks = []
@@ -92,10 +95,6 @@ class TaskDataset(Dataset):
             bert_attention_masks.append(encoded_dict['attention_mask'])
         self.input_ids = torch.cat(input_ids, dim=0)
         self.bert_attention_masks = torch.cat(bert_attention_masks, dim=0)
-
-    def getlabel(self, label, name):
-        label_list = eval(name + '_label_list')
-        return label_list.index(label[0])
 
     def onehot(self, label, name):
         label_list = eval(name + '_label_list')
@@ -116,29 +115,33 @@ class Bert_Model(nn.Module):
         super().__init__()
         self.input_nums = config['trunc_lenth']
         self.output_nums = len(eval(config['name'] + '_label_list'))
-        self.bert = BertForSequenceClassification.from_pretrained(config['bert_path'], num_labels=self.output_nums)
-        self.fc1 = nn.Linear(self.input_nums, self.output_nums)
+        self.bert = BertModel.from_pretrained(config['bert_path'])
+        self.fc1 = nn.Linear(768, self.output_nums)
 
-    def forward(self, input_id, mask, labels):
-        x = self.bert(input_id, attention_mask=mask, labels=labels)
-        return x
+    def forward(self, input_id, mask):
+        out = self.bert(input_id, attention_mask=mask)
+        return torch.sigmoid(self.fc1(out[1]))
 
 
 if __name__ == '__main__':
+    paresr = argparse.ArgumentParser()
+    paresr.add_argument('--task', default='use', type=str)
+    args = paresr.parse_args()
     config = {
         'seed': 1,
         'name': 'form',
         'batch_size': 32,
-        'lr': 0.01,
-        'wd': 0.005,
-        'epoches': 1000,
+        'lr': 5e-5,
+        'wd': 0.0005,
+        'epoches':1000,
         'trunc_lenth': 700,
         'bert_path': 'bert-base-chinese',
-        'patience': 100,
+        'patience': 50,
         'log_dir': './log/'
     }
-    tb_writer = SummaryWriter(config['log_dir'])
+    config['name'] = args.task
 
+    tb_writer = SummaryWriter(config['log_dir'] + config['name'])
     np.random.seed(config['seed'])
     torch.manual_seed(config['seed'])
     torch.cuda.manual_seed_all(config['seed'])
@@ -146,12 +149,6 @@ if __name__ == '__main__':
         train_data = json.load(f)
     with open('dev.json', 'r', encoding='utf-8')as f:
         test_data = json.load(f)
-
-    # config['trunc_lenth'] = int(np.mean(
-    #     [len(re.sub(u"([^\u4e00-\u9fa5\u0030-\u0039\u0041-\u005a\u0061-\u007a])", "", i['fact'])) for i in
-    #      train_data]) + 0.99)
-
-    # print(config['trunc_lenth'])
 
     traindataset = TaskDataset(train_data, config)
     testdataset = TaskDataset(test_data, config)
@@ -161,11 +158,11 @@ if __name__ == '__main__':
     traindataset, validdataset = torch.utils.data.random_split(traindataset, [train_size, valid_size])
 
     trainData = DataLoader(traindataset, batch_size=config['batch_size'], shuffle=True)
-    validData = DataLoader(validdataset, batch_size=config['batch_size'], shuffle=False)
+    validData = DataLoader(validdataset, batch_size=config['batch_size'], shuffle=True)
     testData = DataLoader(testdataset, batch_size=config['batch_size'], shuffle=False)
 
     model = Bert_Model(config).cuda()
-    criterion = nn.CrossEntropyLoss()
+    criterion = nn.BCELoss()
     optimizer = optim.Adagrad(model.parameters(), lr=config['lr'], weight_decay=config['wd'])
 
     best_loss = 1000000
@@ -178,27 +175,28 @@ if __name__ == '__main__':
             break
         model.train()
         train_losses, valid_losses = [], []
-        n, m = 0, 0
         for input_ids, attention_masks, labels in trainData:
-            input_ids, attention_masks, labels = input_ids.cuda(), attention_masks.cuda(), labels.long().cuda()
-            n += 1
+            input_ids, attention_masks, labels = input_ids.cuda(), attention_masks.cuda(), labels.float().cuda()
             model.zero_grad()
-            score = model(input_ids, attention_masks, labels)[1]
+            score = model(input_ids, attention_masks)
             train_loss = criterion(score, labels)
             train_loss.backward()
             optimizer.step()
             train_losses.append(train_loss.cpu().data)
 
-        # 模型评估
+            # 模型评估
         model.eval()
         with torch.no_grad():
             for input_ids, attention_masks, labels in validData:
-                input_ids, attention_masks, labels = input_ids.cuda(), attention_masks.cuda(), labels.long().cuda()
-                m += 1
-                valid_score = model(input_ids, attention_masks, labels)[1]
+                input_ids, attention_masks, labels = input_ids.cuda(), attention_masks.cuda(), labels.float().cuda()
+                valid_score = model(input_ids, attention_masks)
                 valid_loss = criterion(valid_score, labels)
                 valid_losses.append(valid_loss.cpu().data)
-                label_pred=torch.argmax(valid_score.cpu().data, dim=1)
+                label_pred = torch.zeros(size=labels.size())
+                v = valid_score.cpu().data
+                max_ = torch.max(v, dim=1)[0].unsqueeze(dim=1).expand(-1, labels.size(1))
+                label_pred = torch.where(max_ - v <= 0.1, 1, 0)
+
                 f1 = f1_score(labels.cpu().data, label_pred, average='macro')
 
         epoch_iter.set_description('epoch: %d, train loss: %.4f, test loss: %.4f, f1: %.2f,patience: %d' %
@@ -213,18 +211,21 @@ if __name__ == '__main__':
             best_loss = np.mean(valid_losses)
             patience = 0
 
+    torch.save(best_model, './model/' + args.task)
     model = best_model
     model.eval()
-    label_pred = []
     test_losses = []
     with torch.no_grad():
         for input_ids, attention_masks, labels in testData:
-            input_ids, attention_masks, labels = input_ids.cuda(), attention_masks.cuda(), labels.long().cuda()
-            m += 1
-            test_score = model(input_ids, attention_masks, labels)[1]
+            input_ids, attention_masks, labels = input_ids.cuda(), attention_masks.cuda(), labels.float().cuda()
+            test_score = model(input_ids, attention_masks)
             test_loss = criterion(test_score, labels)
-            test_losses.append(valid_loss.cpu().data)
-            label_pred.append(torch.argmax(valid_score.cpu().data, dim=1))
-            f1 = f1_score(labels, label_pred, average='macro')
+            test_losses.append(test_loss.cpu().data)
+            label_pred = torch.zeros(size=labels.size())
+            v = test_score.cpu().data
+            max_ = torch.max(v, dim=1)[0].unsqueeze(dim=1).expand(-1, labels.size(1))
+            label_pred = torch.where(max_ - v <= 0.1, 1, 0)
+
+            f1 = f1_score(labels.cpu().data, label_pred, average='macro')
 
     print('loss: {},f1: {}'.format(np.mean(test_losses), f1))
